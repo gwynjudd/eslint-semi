b3447cacca28fb137910369fb3ad2092dcbb62ad
Gwyn Judd <gjudd@stayinfront.com>
2023-05-16 16:54:06 +1200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	plugins: ['@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
+	root: true,
+	ignorePatterns: [
+		".eslintrc.cjs",
+		"vendor/*",
+		"*.js",
+		"*.d.ts"
+	],
+	rules: {
+		"@typescript-eslint/no-extra-semi": "off",
+		"semi": ["error", "always"],
+	}
+};
